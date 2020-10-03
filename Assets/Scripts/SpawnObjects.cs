d7ac51de7b9e3f2a24684a8ecbb492d6c7a70a4a
Griffin Kupsaw <griffin_kupsaw@brown.edu>
2020-10-03 15:50:31 -0400

    add floating objects

---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public float m_radius = 2.5f;
+    public int m_destroy_time = 4;
+    public int m_instantiate_delay = 100;
+    public GameObject m_obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % m_instantiate_delay == 0) {
+            GameObject objectInstance = Instantiate(
+                m_obj,
+                new Vector3(Random.Range(-m_radius, m_radius), 0, Random.Range(-m_radius, m_radius)),
+                Quaternion.identity
+            );
+            Destroy(objectInstance, m_destroy_time);
+        }
+    }
+}
