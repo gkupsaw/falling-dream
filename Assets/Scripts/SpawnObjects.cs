e10fb8e61858c6bb7fb6e4aede16d211f201d816
Griffin Kupsaw <griffin_kupsaw@brown.edu>
2020-10-10 18:14:27 -0400

    finish floating object demo

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class SpawnObjects : MonoBehaviour
 {
-    public float m_radius = 2.5f; // radius of area in x that objects can appear in
-    public int m_destroy_time = 4; // ms
-    public int m_objs_per_sec = 1; // ms
-    public float m_speed = 80f;
-    public GameObject[] m_possible_objects;
+    public float m_radius = 5f; // radius of area in x that objects can appear in
+    public int m_objsPerSec = 1; // ms
+    public float m_speed = 10f;
+    public float m_torque = 10f;
+    public GameObject[] m_possibleObjects;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +18,21 @@ public class SpawnObjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.frameCount % (m_objs_per_sec * 100) == 0) {
-            GameObject obj = m_possible_objects[Random.Range(0, m_possible_objects.Length)];
+        if (Time.frameCount % (m_objsPerSec * 100) == 0) {
+            GameObject obj = m_possibleObjects[Random.Range(0, m_possibleObjects.Length)];
             GameObject objectInstance = Instantiate(
                 obj,
                 new Vector3(Random.Range(-m_radius, m_radius), 0, Random.Range(-m_radius, m_radius)),
                 Quaternion.identity
             );
-            Rigidbody rBody = objectInstance.GetComponent<Rigidbody>();
-            rBody.AddForce(objectInstance.transform.up * m_speed);
-            Destroy(objectInstance, m_destroy_time);
+
+            Rigidbody rb = objectInstance.GetComponent<Rigidbody>();
+            float timeAlive = transform.position.y / m_speed;
+            float thrust = rb.mass * m_speed;
+            rb.AddForce(objectInstance.transform.up * thrust, ForceMode.Impulse);
+            rb.AddTorque(new Vector3(Random.Range(0, m_torque), Random.Range(0, m_torque), Random.Range(0, m_torque)));
+
+            Destroy(objectInstance, timeAlive);
         }
     }
 }
