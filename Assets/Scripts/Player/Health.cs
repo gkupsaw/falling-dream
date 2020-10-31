37470ef6e283630885df3a8760c852ea5baee2e6
Griffin Kupsaw <griffin_kupsaw@brown.edu>
2020-10-31 16:23:25 -0400

    add lives system

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace FallingDream.Player {
+    public class Health : MonoBehaviour
+    {
+        public int health = 3;
+        public Text healthTxt;
+
+        void Start() {
+            healthTxt.text = "Lives: " + health;
+        }
+
+        public void TakeDamage() {
+            if (health > 0) {
+                health--;
+                healthTxt.text = "Lives: " + health;
+            }
+            if (health == 0) {
+                Debug.Log("Gameover");
+            }
+        }
+    }
+}
