275e121b30f62d4a49daaf45a396e2aaa717f6c0
Griffin Kupsaw <griffin_kupsaw@brown.edu>
2020-11-07 17:51:49 -0500

    add gradient

---
+++ Assets/Scripts/System/GradientScroll.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FallingDream.System {
+    public class GradientScroll : MonoBehaviour
+    {
+        public Color[] rgbaList;
+        public float secondsPerCycle = 10f;
+        public Material mat;
+        private Gradient grad;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GenGradient();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            EvalGradient();
+        }
+
+        void GenGradient()
+        {
+            if (rgbaList.Length > 0)
+            {
+                int numColors = rgbaList.Length;
+                int numGradColors = numColors + 1;
+                grad = new Gradient();
+                GradientColorKey[] gck = new GradientColorKey[numGradColors];
+                GradientAlphaKey[] gak = new GradientAlphaKey[numGradColors];
+
+                float timeStep = 1f / numGradColors;
+                for (int i = 0 ; i < numColors ; i++)
+                {
+                    float time = i * timeStep;
+                    gck[i].color = rgbaList[i];
+                    gck[i].time = time;
+                    gak[i].alpha = rgbaList[i].a;
+                    gak[i].time = time;
+                }
+                gck[numColors].color = rgbaList[0];
+                gck[numColors].time = 1f;
+                gak[numColors].alpha = rgbaList[0].a;
+                gak[numColors].time = 1f;
+
+                grad.SetKeys(gck, gak);
+            }
+        }
+
+        void EvalGradient()
+        {
+            float timeStep = Time.time % secondsPerCycle / secondsPerCycle;
+            mat.color = grad.Evaluate(timeStep);
+            RenderSettings.skybox = mat;
+            // foreach (Transform child in transform)
+            // {
+            //     child.GetComponent<Renderer>().material.color = grad.Evaluate(timeStep);
+            // }
+        }
+    }
+}
