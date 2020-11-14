1063cca2d6cf3a4c3e09603c05a4e515fd5556f1
JMigneault <jacrovm@gmail.com>
2020-11-14 14:27:45 -0500

    Add to menus and add lives UI.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,21 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace FallingDream.Player {
     public class PlayerHealth : MonoBehaviour
     {
         public int health = 3;
         public Text healthTxt;
+        public Image[] healthImages; 
+        public Sprite healthLostSprite;
+        public bool useImages;
+
+        void Update() { // TEMP
+            if (Input.GetKeyDown(KeyCode.U)) {
+                TakeDamage();
+            }
+        }
 
         void Start() {
-            healthTxt.text = "Lives: " + health;
+            Assert.IsTrue(healthImages.Length == health);
+            if (!useImages) {
+                healthTxt.text = "Lives: " + health;
+                for (int i = 0; i < healthImages.Length; i++) {
+                    healthImages[i].gameObject.SetActive(false);
+                }
+            }
         }
 
         public void TakeDamage() {
             if (health > 0) {
                 health--;
-                healthTxt.text = "Lives: " + health;
+                if (useImages) {
+                    healthImages[health].sprite = healthLostSprite;
+                } else {
+                    healthTxt.text = "Lives: " + health;
+                }
             }
             if (health == 0) {
                 Debug.Log("Gameover");
