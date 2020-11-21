7cb4b60f1b9fbc6e23d64bff941db693d47bd35e
JMigneault <jacrovm@gmail.com>
2020-11-21 01:40:51 -0500

    Add invisible wall shenanigans w/ shaders.

---
+++ Assets/Scripts/System/InvisibleWallController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FallingDream.System {
+
+    public class InvisibleWallController : MonoBehaviour
+    {
+        public Material invisibleWallMaterial;
+
+        void Update() {
+            Vector3 p = this.transform.position;
+            invisibleWallMaterial.SetVector("_PlayerPos", new Vector4(p.x, p.y, p.z, 1));
+        }
+
+    }
+}
